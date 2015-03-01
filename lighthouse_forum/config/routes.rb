96b041888627e8cb5ce31033cf2b759b4cfc242c
Andrew Good <drewhgood@gmail.com>
2015-03-01 15:53:43 -0800

    post model and migration created.

--- lighthouse_forum/config/routes.rb
+++ lighthouse_forum/config/routes.rb
@@ -1,6 +1,5 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
-
-  # [tons of helpful comments]
+   get 'posts', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show'
 end
